2a625e2bcb6a48fcdf3b349e555057b36b8c1062
deserve82 <inth8204@gmail.com>
2021-11-03 18:25:25 +0900

    [fix] change model and add migrations

--- url/models.py
+++ url/models.py
@@ -8,3 +8,10 @@ class Url(models.Model):
     shortend_url = models.URLField(default="", max_length=8, null=False)
     create_at = models.DateTimeField(default=datetime.datetime.now(), null=False)
     clicked_count = models.IntegerField(default=0, null=False)
+
+
+class ResultModel:
+    def __init__(self, result_code=0, message="", data=None):
+        self.result_code = result_code
+        self.result_msg = message
+        self.data = data
