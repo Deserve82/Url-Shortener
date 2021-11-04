4c3ebde6d450890f2ccf66f5238f08e0b52b6b09
Kangho LEE <inth8204@gmail.com>
2021-11-04 18:30:29 +0900

    [feat] add comment utils functions

--- url/utils.py
+++ url/utils.py
@@ -6,11 +6,16 @@ from django.conf import settings
 
 from url.models import Url
 
-
 logger = logging.getLogger(__name__)
 
 
 def get_or_none(class_model, **kwargs):
+    """
+    객체가 있을 때에만 object를 반환하는 함수입니다. 아니라면 None을 반환합니다.
+    :param class_model: Model Class
+    :param kwargs: key, value arguments
+    :return: object or None
+    """
     try:
         return class_model.objects.get(**kwargs)
     except class_model.MultipleObjectsReturned as e:
@@ -21,6 +26,10 @@ def get_or_none(class_model, **kwargs):
 
 
 def make_shorten_url():
+    """
+    무작위 8자와 함께 현재 페이지의 주소를 함께 string형식으로 return 합니다.
+    :return: string
+    """
     chars = settings.VALID_CHARS
     while True:
         new_url_key = "".join(random.sample(chars, 8))
@@ -55,6 +64,11 @@ def make_url_scheme(url):
 
 
 def check_valid_domain(url):
+    """
+    입력 받은 url 빈 것인지 아니면 현재 도메인이 들어가있는지 확인합니다.
+    :param: string
+    :return: bool
+    """
     result = urlparse(url)
     if result.netloc == "" or result.netloc == settings.CURRENT_DOMAIN:
         return False
@@ -62,14 +76,28 @@ def check_valid_domain(url):
 
 
 def get_current_url():
+    """
+    현재 url 주소를 반환 합니다.
+    :return: string
+    """
     return settings.CURRENT_SCHEME + "://" + settings.CURRENT_DOMAIN
 
 
 def get_shorten_url(url_key):
+    """
+    현재 url 주소를 key와 함께 주소로 반환합니다.
+    :param url_key: string (size 8)
+    :return: string
+    """
     return settings.CURRENT_SCHEME + "://" + settings.CURRENT_DOMAIN + "/" + url_key
 
 
 def check_url_key_valid(url_key):
+    """
+    입력받은 url_key가 유효한지 확인합니다.
+    :param url_key: string (size 8)
+    :return: bool
+    """
     result = True
     if len(url_key) != 8:
         result = False
